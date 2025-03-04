236a24ec0a75df3c7e01d4b594cb2ab1f838b085
Kirill Malskiy <sabbat@mail.ru>
2025-03-04 10:50:06 +0300

    добавил модели классов для эндпоинта /hubs

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/model/hub/DeviceAddedEvent.java
@@ -0,0 +1,22 @@
+package ru.yandex.practicum.model.hub;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import ru.yandex.practicum.model.hub.enums.DeviceType;
+import ru.yandex.practicum.model.hub.enums.HubEventType;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class DeviceAddedEvent extends HubEvent{
+    @NotBlank
+    private String id;
+    private DeviceType deviceType;
+
+    @Override
+    public HubEventType getType() {
+        return HubEventType.DEVICE_ADDED;
+    }
+}
---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/model/hub/ScenarioAddedEvent.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.model.hub;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import ru.yandex.practicum.model.hub.enums.HubEventType;
+
+import java.time.Instant;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class ScenarioAddedEvent extends HubEvent{
+    @NotBlank
+    @Size(min = 3)
+    private String name;
+    @NotBlank
+    private List<ScenarioCondition> conditions;
+    @NotBlank
+    private List<DeviceAction> actions;
+
+    @Override
+    public HubEventType getType() {
+        return HubEventType.SCENARIO_ADDED;
+    }
+}
