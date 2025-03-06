2cfa662a3fce3941ff5be6a0e16f743d750b682d
Kirill Malskiy <sabbat@mail.ru>
2025-03-06 10:23:34 +0300

    добавил класс KafkaProducerConfig с бином getProducer

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/config/KafkaProducerConfig.java
@@ -0,0 +1,25 @@
+package ru.yandex.practicum.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Properties;
+
+@RequiredArgsConstructor
+public class KafkaProducerConfig {
+    private final KafkaPropertiesConfig kafkaPropertiesConfig;
+
+    @Bean
+    public Producer<String, SpecificRecordBase> getProducer() {
+        Properties config = new Properties();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaPropertiesConfig.getBootstrapService());
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, kafkaPropertiesConfig.getKeySerializer());
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, kafkaPropertiesConfig.getValueSerializer());
+
+        return new KafkaProducer<>(config);
+    }
+}
