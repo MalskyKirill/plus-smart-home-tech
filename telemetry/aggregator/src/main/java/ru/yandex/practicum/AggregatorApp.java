a95a4ef9a75da2437f3cb3d3e368712514aea5e1
Kirill Malskiy <sabbat@mail.ru>
2025-03-23 21:17:21 +0300

    добавил AggregatorEventHandler

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/AggregatorApp.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/AggregatorApp.java
@@ -1,4 +1,10 @@
 package ru.yandex.practicum;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AggregatorApp {
+    public static void main(String[] args) {
+
+    }
 }
---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/starter/AggregationStarter.java
@@ -0,0 +1,4 @@
+package ru.yandex.practicum.starter;
+
+public class AggregationStarter {
+}
