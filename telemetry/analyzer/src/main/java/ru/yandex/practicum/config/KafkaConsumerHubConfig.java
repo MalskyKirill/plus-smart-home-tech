80bbc0d53daf16dc96b7a1ce28d07f9981031934
Kirill Malskiy <sabbat@mail.ru>
2025-03-26 16:36:08 +0300

    добавил бины для получения консюмеров

---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/config/KafkaConsumerHubConfig.java
@@ -0,0 +1,30 @@
+package ru.yandex.practicum.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+
+import java.util.Properties;
+
+@Configuration
+@RequiredArgsConstructor
+public class KafkaConsumerHubConfig {
+    private final KafkaPropertiesConfig kafkaPropertiesConfig;
+
+    @Bean
+    public KafkaConsumer<String, HubEventAvro> getHubEventConsumer() {
+        Properties config = new Properties();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaPropertiesConfig.getBootstrapService());
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaPropertiesConfig.getConsumerHubGroupId());
+        config.put(ConsumerConfig.CLIENT_ID_CONFIG, kafkaPropertiesConfig.getConsumerHubClientIdConfig());
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, kafkaPropertiesConfig.getConsumerHubKeyDeserializer());
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, kafkaPropertiesConfig.getConsumerHubValueDeserializer());
+
+        return new KafkaConsumer<>(config);
+    }
+
+
+}
