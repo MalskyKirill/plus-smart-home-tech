576581eb9fcdfbd3cc75124a204159dafa9477a7
Kirill Malskiy <sabbat@mail.ru>
2025-03-28 11:35:49 +0300

    запустил в отдельном потоке обработчик событий от пользовательских хабов

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/AnalyzerApp.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/AnalyzerApp.java
@@ -1,10 +1,22 @@
 package ru.yandex.practicum;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.yandex.practicum.processor.HubEventProcessor;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class AnalyzerApp {
     public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(AnalyzerApp.class, args);
+
+        final HubEventProcessor hubEventProcessor = context.getBean(HubEventProcessor.class);
+
+        Thread hubEventsThread = new Thread(hubEventProcessor);
+        hubEventsThread.setName("HubEventHandlerThread");
+        hubEventsThread.start();
 
     }
 }
