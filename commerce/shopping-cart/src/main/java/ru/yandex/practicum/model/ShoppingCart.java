f928392b9ff696381d9f11fabde269f42570dc64
Kirill Malskiy <sabbat@mail.ru>
2025-04-14 14:56:51 +0300

    в модуль shopping-cart добавил модель ShoppingCart

---
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/model/ShoppingCart.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Map;
+import java.util.UUID;
+
+
+@Entity
+@Table(name = "cart")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class ShoppingCart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "cart_id")
+    UUID cardId;
+    @Column(name = "user_name")
+    String userName;
+    @ElementCollection
+    @CollectionTable(name = "cart_products", joinColumns = @JoinColumn(name = "cart_id"))
+    @MapKeyColumn(name = "product_id")
+    @Column(name = "quantity")
+    Map<UUID, Long> products;
+}
