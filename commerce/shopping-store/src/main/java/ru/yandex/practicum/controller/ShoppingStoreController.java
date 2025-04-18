09b2f710e144f546904a696fac23ff0f2a390e85
Kirill Malskiy <sabbat@mail.ru>
2025-04-18 11:49:17 +0300

    разработал интерфейс Feing-клиента для модуля shopping-store и реализовал его в контроллере

--- commerce/shopping-store/src/main/java/ru/yandex/practicum/controller/ShoppingStoreController.java
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/controller/ShoppingStoreController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.controller;
 
+import client.ShoppingStoreClient;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,40 +18,40 @@ import java.util.UUID;
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/shopping-store")
-public class ShoppingStoreController {
+public class ShoppingStoreController implements ShoppingStoreClient {
     private final ShoppingStoreService shoppingStoreService;
 
-    @PutMapping
+    @Override
     public ProductDto createProduct(@Valid @RequestBody ProductDto productDto) {
         log.info("PUT-запрос к эндпоинту: '/api/v1/shopping-store' на создание товара c productName = {}", productDto.getProductName());
         return shoppingStoreService.create(productDto);
     }
 
-    @GetMapping("/{productId}")
+    @Override
     public ProductDto getProductById(@PathVariable UUID productId) {
         log.info("GET-запрос к эндпоинту: '/api/v1/shopping-store/productId' на получение товара c productId = {}", productId);
         return shoppingStoreService.getProduct(productId);
     }
 
-    @PostMapping
+    @Override
     public ProductDto updateProduct(@RequestBody ProductDto productDto) {
         log.info("POST-запрос к эндпоинту: '/api/v1/shopping-store' на обновление товара c productName = {}", productDto.getProductName());
         return shoppingStoreService.update(productDto);
     }
 
-    @PostMapping("/removeProductFromStore")
+    @Override
     public boolean deleteProduct(@RequestBody UUID productId) {
         log.info("POST-запрос к эндпоинту: '/api/v1/shopping-store/removeProductFromStore' на удаление товара c productId = {}", productId);
         return shoppingStoreService.delete(productId);
     }
 
-    @PostMapping("/quantityState")
+    @Override
     public boolean setProductQuantityState(@Valid @RequestBody SetProductQuantityStateRequest request) {
         log.info("POST-запрос к эндпоинту: '/api/v1/shopping-store/quantityState' на изменение количества товара c productId = {}", request.getProductId());
         return shoppingStoreService.setQuantityState(request);
     }
 
-    @GetMapping
+    @Override
     public List<ProductDto> getProductsByCategory(@RequestParam ProductCategory category, Pageable pageable) {
         log.info("GET-запрос к эндпоинту: '/api/v1/shopping-store' на получение списка товаров по типу {} в пагинированном виде", category);
         return shoppingStoreService.getProducts(category, pageable);
