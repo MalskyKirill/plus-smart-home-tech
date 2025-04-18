b49ce38b2e2072350f1d7461b1f279b26f9c11ce
Kirill Malskiy <sabbat@mail.ru>
2025-04-18 11:57:48 +0300

    разработал интерфейс Feing-клиента для модуля shopping-cart и реализовал его в контроллере

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/ShoppingCartApp.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/ShoppingCartApp.java
@@ -2,8 +2,11 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ShoppingCartApp {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApp.class, args);
--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/controller/ShoppingCardController.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/controller/ShoppingCardController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.controller;
 
+import client.ShoppingCartClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -15,36 +16,36 @@ import java.util.UUID;
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/shopping-cart")
-public class ShoppingCardController {
+public class ShoppingCardController implements ShoppingCartClient {
     private final ShoppingCartService shoppingCartService;
 
-    @PutMapping
+    @Override
     public ShoppingCartDto addProductsInShoppingCart (@RequestParam String username,
                                                       @RequestBody Map<UUID, Long> products) {
         log.info("PUT-запрос к эндпоинту: '/api/v1/shopping-cart' на добавление товара в корзину");
         return shoppingCartService.addProducts(username, products);
     }
 
-    @GetMapping
+    @Override
     public ShoppingCartDto getActualShoppingCartByUser (@RequestParam String username) {
         log.info("GET-запрос к эндпоинту: '/api/v1/shopping-cart' на получение актуальной корзину для авторизованного пользователя");
         return shoppingCartService.getShoppingCart(username);
     }
 
-    @DeleteMapping
+    @Override
     public void deactivateShoppingCartByUser (@RequestParam String username) {
         log.info("DELETE-запрос к эндпоинту: '/api/v1/shopping-cart' на деактивацию корзины товаров для пользователя");
         shoppingCartService.deactivateShoppingCart(username);
     }
 
-    @PostMapping("/remove")
+    @Override
     public ShoppingCartDto removeProductFromShoppingCart (@RequestParam String username,
                                                           @RequestBody List<UUID> products) {
         log.info("POST-запрос к эндпоинту: '/api/v1/shopping-cart/remove' на удаление товаров из корзины");
         return shoppingCartService.remove(username, products);
     }
 
-    @PostMapping("/change-quantity")
+    @Override
     public ShoppingCartDto changeProductQuantityInShoppingCart (@RequestParam String username,
                                                                 @RequestBody ChangeProductQuantityRequestDto requestDto) {
         log.info("POST-запрос к эндпоинту: '/api/v1/shopping-cart/change-quantity' на изменение товаров в корзины");
